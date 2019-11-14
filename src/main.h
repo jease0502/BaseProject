6e2ff1de77622c6353da49aab8b43219427b4646
Hsu_Chun_Wei <super20110505@gmail.com>
2019-11-14 19:57:21 +0800

    test

--- src/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
-int addNumber(int a, int b);
-int getTriangleType(int a, int b, int c);
-int jease(int a , int b);
-int Yukari(int a , int b);
-#endif // MAIN_H_INCLUDED
