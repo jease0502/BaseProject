b2e826a5a2adad45efb988c57a636bd362755840
ftsjease <ftsjease0502@gmail.com>
2019-12-23 09:23:48 +0800

    php update

--- php_code/list_delete.php
+++ php_code/list_delete.php
@@ -9,13 +9,14 @@ mysqli_select_db($con,$db_name);
 
 $uid = $_GET["uid"];
 $list = $_GET["list"];
+$date = $_GET["date"]
 
 //本帳號	,內容,日期,時間,地點
 
-$sql_query =  "DELETE  提醒 WHERE 本帳號 = '$uid' AND 清單 = '$list' ";
+$sql_query =  "DELETE  提醒 WHERE 本帳號 = '$uid' AND 清單 = '$list' and 日期 = 'date' ";
 	//新增事件
 $result = mysqli_query($con,$sql_query);//執行sql
-$sql_query =  "SELECT 本帳號	,清單  FROM 提醒 WHERE 本帳號 = '$uid' AND 清單 = '$list'";
+$sql_query =  "SELECT 本帳號	,清單,日期  FROM 提醒 WHERE 本帳號 = '$uid' AND 清單 = '$list'";
 	//檢測資料庫是否有對應的username和password的sql
 $result = mysqli_query($con,$sql_query);//執行sql
 $rowsa=mysqli_num_rows($result);//返回一個數值
--- php_code/list_insert.php
+++ php_code/list_insert.php
@@ -9,13 +9,14 @@ mysqli_select_db($con,$db_name);
 
 $uid = $_GET["uid"];
 $list = $_Get["list"]
+$date = $_GET["date"]
 
 //從app get uid and pwd
-$sql_query =  "Insert INTO 提醒 VALUES ('$uid','$list')";
+$sql_query =  "Insert INTO 提醒 VALUES ('$uid','$list','$date')";
 //新增帳密
 $result = mysqli_query($con,$sql_query);//執行sql
 echo "插入".$result;
-$sql_query =  "SELECT 本帳號,清單  FROM 提醒 WHERE 本帳號 = '$uid' and 清單 = 'list' ";
+$sql_query =  "SELECT 本帳號,清單,日期  FROM 提醒 WHERE 本帳號 = '$uid' and 清單 = 'list' and 日期 = 'date' ";
 //檢測資料庫是否有對應的username和password的sql
 $result = mysqli_query($con,$sql_query);//執行sql
 $rowsa=mysqli_num_rows($result);//返回一個數值
